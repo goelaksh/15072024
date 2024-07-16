3e4bc5f64b76358bb6595b1c78058a0b9053c439
AG <AG@yopmail.com>
2024-07-16 17:30:40 +0000

    C# hello file added

---
+++ hello.cs
@@ -0,0 +1,15 @@
+using System; 
+
+namespace HelloWorldApp { 
+    
+    // Class declaration 
+    class Geeks { 
+
+        static void Main(string[] args) { 
+
+            Console.WriteLine("Hello World!"); 
+
+            Console.ReadKey(); 
+        } 
+    } 
+} 
